1d7a49820ea510b2ebfe591ce082a1c5fa8da29d
tanvihegde <84328063+tanvihegde@users.noreply.github.com>
2021-09-20 10:19:15 -0400

    Js

--- app/public/js/user.js
+++ app/public/js/user.js
@@ -8,7 +8,7 @@ var app = new Vue({
         userAge:'',
         userPicLarge:'',
         userPicThumb:''
-    },
+        },
 
     created() {
         this.fetchUser();
@@ -16,13 +16,14 @@ var app = new Vue({
 
     methods: {
         fetchUser: function() {
-            fetch('https://randomuser.me/api/')
+            fetch('https://randomuser.me/')
             .then(response => response.json())
             .then(data => {
                 var userinfo = data.results[0];
                 console.log(userinfo);
                 this.userName = userinfo.name.first + " " + userinfo.name.last;
                 this.userEmail = userinfo.email;
+                this.userCountry = userinfo.location.country;
                 this.userBirthdate = userinfo.dob.date[0] + userinfo.dob.date[1] + userinfo.dob.date[2] + userinfo.dob.date[3] + "/" + userinfo.dob.date[5] + userinfo.dob.date[6] ;
                 this.userAge = userinfo.picture.large;
                 this.userPicThumb = userinfo.picture.thumbnail;
