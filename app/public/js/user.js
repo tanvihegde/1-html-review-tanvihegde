3ac5e6d4b23b40d88c7b2012e531e7864ac0a60f
tanvihegde <84328063+tanvihegde@users.noreply.github.com>
2021-09-20 21:18:33 -0400

    VueJsdraft

--- app/public/js/user.js
+++ app/public/js/user.js
@@ -1,16 +1,3 @@
-// var app = new Vue({
-//     el: '#userapp',
-//     data: {
-//         return 
-//         // userName:'',
-//         // userEmail:'',
-//         // userCountry:'',
-//         // userBirthdate:'',
-//         // userAge:'',
-//         // userPicLarge:'',
-//         // userPicThumb:''
-//         },
-
 const app1 = {
     data(){
         return{
@@ -25,11 +12,7 @@ const app1 = {
             .then(data => {
                 this.userinfo = data.results[0];
                 console.log(userinfo);
-                // this.userName = userinfo.name.first + " " + userinfo.name.last;
-                // this.userEmail = userinfo.email;
-                // this.userCountry = userinfo.location.country;
-                // this.userBirthdate = userinfo.dob.date[0] + userinfo.dob.date[1] + userinfo.dob.date[2] + userinfo.dob.date[3] + "/" + userinfo.dob.date[5] + userinfo.dob.date[6] ;
-                // this.userAge = userinfo.picture.large;
+
                 // this.userPicThumb = userinfo.picture.thumbnail;
             });
         }
