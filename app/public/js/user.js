418da54c77aaf9e4a7a02b8a8ca2e5c06e8e6981
tanvihegde <84328063+tanvihegde@users.noreply.github.com>
2021-09-20 21:05:10 -0400

    VueJs Hw draft

--- app/public/js/user.js
+++ app/public/js/user.js
@@ -1,33 +1,43 @@
-var app = new Vue({
-    el: '#userapp',
-    data: {
-        userName:'',
-        userEmail:'',
-        userCountry:'',
-        userBirthdate:'',
-        userAge:'',
-        userPicLarge:'',
-        userPicThumb:''
-        },
+// var app = new Vue({
+//     el: '#userapp',
+//     data: {
+//         return 
+//         // userName:'',
+//         // userEmail:'',
+//         // userCountry:'',
+//         // userBirthdate:'',
+//         // userAge:'',
+//         // userPicLarge:'',
+//         // userPicThumb:''
+//         },
 
-    created() {
-        this.fetchUser();
+const app1 = {
+    data(){
+        return{
+            "userinfo":{},
+        }
     },
 
-    methods: {
-        fetchUser: function() {
-            fetch('https://randomuser.me/')
+    methods:{
+        fetchUser(){
+            fetch('https://randomuser.me/api/')
             .then(response => response.json())
             .then(data => {
-                var userinfo = data.results[0];
+                this.userinfo = data.results[0];
                 console.log(userinfo);
-                this.userName = userinfo.name.first + " " + userinfo.name.last;
-                this.userEmail = userinfo.email;
-                this.userCountry = userinfo.location.country;
-                this.userBirthdate = userinfo.dob.date[0] + userinfo.dob.date[1] + userinfo.dob.date[2] + userinfo.dob.date[3] + "/" + userinfo.dob.date[5] + userinfo.dob.date[6] ;
-                this.userAge = userinfo.picture.large;
-                this.userPicThumb = userinfo.picture.thumbnail;
+                // this.userName = userinfo.name.first + " " + userinfo.name.last;
+                // this.userEmail = userinfo.email;
+                // this.userCountry = userinfo.location.country;
+                // this.userBirthdate = userinfo.dob.date[0] + userinfo.dob.date[1] + userinfo.dob.date[2] + userinfo.dob.date[3] + "/" + userinfo.dob.date[5] + userinfo.dob.date[6] ;
+                // this.userAge = userinfo.picture.large;
+                // this.userPicThumb = userinfo.picture.thumbnail;
             });
         }
-    }
-})
+},
+
+
+created() {
+    this.fetchUser();
+},
+}
+Vue.createApp(app1).mount('#userapp');
