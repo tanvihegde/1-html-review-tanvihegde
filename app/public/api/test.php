c00ff7b9743567038e5778fc2c7e8431ebafab47
tanvihegde <84328063+tanvihegde@users.noreply.github.com>
2021-09-27 11:31:17 -0400

    php

---
+++ app/public/api/test.php
@@ -0,0 +1,30 @@
+<?php
+
+$foo= "To be";
+$num =2;
+$bar = "or not to be";
+
+echo $foo ." ". $bar; #to print something out \
+echo "\n";
+echo $num * $num *$num;
+
+$array = [1,2,3,4,5];
+
+$array2 = [
+    "first" => "Tanvi",
+    "second" => "class",
+    "best" => "DS"
+];
+
+if (true) {
+    echo "TRUE \n";
+} else {
+    echo "FALSE \n";
+}
+
+// $arr as json 
+
+echo json_encode ($arr);
+
+# PHP and JS: CamelCase
+#Constants: CAPITAL 
