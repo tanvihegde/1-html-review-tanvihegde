920eea1eab26bafa01a43fd96dea6eafa4aa13a4
tanvihegde <84328063+tanvihegde@users.noreply.github.com>
2021-10-10 22:22:05 -0400

    j

--- app/public/api/student/index.php
+++ app/public/api/student/index.php
@@ -6,7 +6,7 @@ require 'class/DbConnection.php';
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM students';
+$sql = 'SELECT * FROM books';
 $vars = [];
 
 // if (isset($_GET['guid'])) {
---
+++ app/public/js/index.js
@@ -0,0 +1,60 @@
+const SomeApp = {
+    data() {
+      return {
+        students: [],
+        selectedStudent: null,
+        offers: []
+      }
+    },
+    computed: {},
+    methods: {
+        prettyData(d) {
+            return dayjs(d)
+            .format('D MMM YYYY')
+        },
+        prettyDollar(n) {
+            const d = new Intl.NumberFormat("en-US").format(n);
+            return "$ " + d;
+        },
+        selectStudent(s) {
+            if (s == this.selectedStudent) {
+                return;
+            }
+            this.selectedStudent = s;
+            this.offers = [];
+            this.fetchOfferData(this.selectedStudent);
+        },
+        fetchStudentData() {
+            fetch('/api/student/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.students = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        },
+        fetchOfferData(s) {
+            console.log("Fetching offer data for ", s);
+            fetch('/api/offer/?student=' + s.id)
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.offers = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+            .catch( (error) => {
+                console.error(error);
+            });
+        }
+    },
+    created() {
+        this.fetchStudentData();
+    }
+  
+  }
+  
+  Vue.createApp(SomeApp).mount('#offerApp');
